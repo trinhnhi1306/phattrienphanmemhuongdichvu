2a159911c33ae680efcd94fe583a7d64b8c05eec
phucpvz <senaarata12345@gmail.com>
2022-02-16 20:50:23 +0700

    Đăng ký - đăng nhập

--- Web API/MiniStore.WebAPI/MiniStore.WebAPI/App_Start/WebApiConfig.cs
+++ Web API/MiniStore.WebAPI/MiniStore.WebAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,6 +20,14 @@ namespace MiniStore.WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling 
+                = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling 
+                = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver 
+                = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
---
+++ Web API/MiniStore.WebAPI/MiniStore.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using MiniStore.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace MiniStore.WebAPI.Controllers
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiController
+    {
+        VANPHONGPHAMEntities db = new VANPHONGPHAMEntities();
+
+        private User ToJsonUser(User user)
+        {
+            user.Address = null;
+            user.Carts = null;
+            user.Feedbacks = null;
+            user.Orders = null;
+            user.Permission = null;
+            return user;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public User Login([FromBody] User tmp)
+        {
+            User user = db.Users.FirstOrDefault(u => u.username == tmp.username);
+            if (user == null)
+            {
+                return new User();
+            }
+            if (BCrypt.Net.BCrypt.Verify(tmp.password, user.password))
+            {
+                return ToJsonUser(user);
+            }
+            return new User();
+        }
+
+        [HttpPost]
+        [Route("signup")]
+        public string SignUp([FromBody] User user)
+        {
+            if (db.Users.Any(u => u.username == user.username))
+            {
+                return "Tên đăng nhập đã tồn tại!";
+            }
+            if (db.Users.Any(u => u.email == user.email))
+            {
+                return "Email này đã được dùng để đăng ký tài khoản!";
+            }
+
+            user.password = BCrypt.Net.BCrypt.HashPassword(user.password);
+            user.created_at = DateTime.Now;
+            user.permission_id = 1;
+            user.status = true;
+            try
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return "success";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        // PUT: api/Login/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Login/5
+        public void Delete(int id)
+        {
+        }
+
+
+    }
+}
