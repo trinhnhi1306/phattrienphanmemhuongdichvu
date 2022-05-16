a29b085d49bcaa0cf552ff51672b076042921394
trinhnhi1306 <yayoi1306@gmail.com>
2022-05-17 00:35:08 +0700

    Add report API

--- TryJWT/src/main/java/com/Quan/TryJWT/repository/OrderRepository.java
+++ TryJWT/src/main/java/com/Quan/TryJWT/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.Quan.TryJWT.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.Quan.TryJWT.model.Order;
@@ -11,4 +14,6 @@ import com.Quan.TryJWT.model.OrderStatus;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
 	public List<Order> findAllByStatusId(OrderStatus status);
+	public long countByDate(Date date);
+	public long countByStatusId(OrderStatus statusId);
 }
