7de7ed93bf2531bc32b4f7d6b26a193b3fba4bf1
Min Min <hamok17c1dc4@gmail.com>
2022-06-12 15:42:44 +0700

    Lấy user theo status kèm số đơn hàng, list địa chỉ

--- TryJWT/src/main/java/com/Quan/TryJWT/repository/UserRepository.java
+++ TryJWT/src/main/java/com/Quan/TryJWT/repository/UserRepository.java
@@ -15,19 +15,26 @@ import com.Quan.TryJWT.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	public Optional<User> findByUsername(String username);
 
+	public Optional<User> findById(Long id);
+
+	public Integer countByStatus(Boolean status);
+
 	public Boolean existsByUsername(String username);
 
 	public Boolean existsByEmail(String email);
 
 	public List<User> findAllByStatus(boolean status, Pageable pageable);
-	public List<User> findAllByStatus(boolean status);
-	Boolean existsByPhone(String phone);
-	
-	@Query(value = "select u.* from users u where u.email = :email and u.username NOT LIKE :username ", nativeQuery = true)
-	public List<User> verifyDuplicateEmail(@Param("email") String email, @Param("username") String username); 
-	
-	@Query(value = "select u.* from users u where u.phone = :phone and u.username NOT LIKE :username ", nativeQuery = true)
-	public List<User> verifyDuplicatePhone(@Param("phone") String phone, @Param("username") String username); 
-	
 
+	public List<User> findAllByStatus(boolean status);
+
+	public Boolean existsByPhone(String phone);
+
+	@Query(value = "select u.* from users u where u.email = :email and u.username NOT LIKE :username ", nativeQuery = true)
+	public List<User> verifyDuplicateEmail(@Param("email") String email, @Param("username") String username);
+
+	@Query(value = "select u.* from users u where u.phone = :phone and u.username NOT LIKE :username ", nativeQuery = true)
+	public List<User> verifyDuplicatePhone(@Param("phone") String phone, @Param("username") String username);
+
+	@Query(value = "SELECT COUNT(order_id) FROM orders WHERE user_id = :idUser GROUP BY user_id", nativeQuery = true)
+	public int getNumberOrderById(@Param("idUser") long idUser);
 }
